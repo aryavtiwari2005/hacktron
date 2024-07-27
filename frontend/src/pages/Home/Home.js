92d0b0fea96c6902bca7474864f46bed8cd2a6dd
Aryav Tiwari <aryavtiwari2005@gmail.com>
2024-07-27 19:23:38 +0530

    homepage

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -3,49 +3,53 @@ import { RiNetflixLine, RiProductHuntLine } from "react-icons/ri";
 import { Link } from "react-router-dom";
 import "./Home.scss";
 import heroImg from "../../assets/inv-img.png";
+import homeVideo from "../../assets/HomeScreenVideo.mp4";
 import imcLogo from "../../assets/indore_municipal.png"
 import { ShowOnLogin, ShowOnLogout } from "../../components/protect/HiddenLink";
 
 const Home = () => {
   return (
     <div className="home">
-      <nav className="container --flex-between ">
+      <video id="background-video" lopp autoPlay muted>
+        <source src={homeVideo} type="video/mp4" />
+      </video>
+      <nav className="container">
         <div className="nav-bar">
-            <div className="logo">
-               <img src={imcLogo}></img>
-            </div>
-            <div className="t-b">
-              <div className="logo-texts">
-                <div className="initiative">
-                  <h2>STOCK PILE INDORE</h2><br></br>
-                  <h4 className="invent-text">Inventory Management System</h4>
-                </div>
+          <div className="logo">
+            <img src={imcLogo}></img>
+          </div>
+          <div className="t-b">
+            <div className="logo-texts">
+              <div className="initiative">
+                <h2>STOCK PILE INDORE</h2><br></br>
+                <h4 className="invent-text">Inventory Management System</h4>
+              </div>
             </div>
 
-           <ul className="home-links">
-            <ShowOnLogout>
-              <li>
-              <button className="--btn --btn-primary">
-                <Link to="/login">Login</Link>
-              </button>
-            </li>
-            </ShowOnLogout>
-            <ShowOnLogin>
-            <li>
-              <button className="--btn --btn-primary">
-                <Link to="/dashboard">Dashboard</Link>
-              </button>
-            </li>
-          </ShowOnLogin>
-        </ul>
-        </div>
+            <ul className="home-links">
+              <ShowOnLogout>
+                <li>
+                  <button className="--btn --btn-primary">
+                    <Link to="/login">Login</Link>
+                  </button>
+                </li>
+              </ShowOnLogout>
+              <ShowOnLogin>
+                <li>
+                  <button className="--btn --btn-primary">
+                    <Link to="/dashboard">Dashboard</Link>
+                  </button>
+                </li>
+              </ShowOnLogin>
+            </ul>
+          </div>
         </div>
       </nav>
       {/* HERO SECTION */}
       <section className="container hero">
         <div className="hero-text">
           <p>
-            The Indore Municipal Corporation Inventory Management System streamlines resource tracking and allocation, reduces costs, improves efficiency, 
+            The Indore Municipal Corporation Inventory Management System streamlines resource tracking and allocation, reduces costs, improves efficiency,
             and provides real-time insights for informed decision-making, ensuring optimal municipal operations and sustainability.
           </p>
           <div className="hero-buttons">
