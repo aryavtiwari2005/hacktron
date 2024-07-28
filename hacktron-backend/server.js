c845b7e6aafd5bd411f97adba75959dd6dc698a9
Aryav Tiwari <58284558+aryavtiwari2005@users.noreply.github.com>
2024-07-28 10:36:57 +0530

    Update server.js

--- hacktron-backend/server.js
+++ hacktron-backend/server.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(
     cors({
-        origin: ["http://localhost:3000"],
+        origin: ["http://localhost:3000", "https://hacktron.vercel.app"],
         credentials: true,
     })
 )
